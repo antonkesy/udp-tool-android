5cff3ec3eb9da9a96119cfcac80f4cd47320765c
Anton Kesy <anton@kesy.de>
2021-09-01 21:01:31 +0200

    Create RemoteView.kt

---
+++ app/src/main/java/com/antonkesy/udptool/ui/RemoteView.kt
@@ -0,0 +1,23 @@
+package com.antonkesy.udptool.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RemoteContent(label: String, modifier: Modifier) {
+    Column(modifier = modifier) {
+        CardHeader(label)
+        Row {
+            Text(text = "IP")
+            TextField(value = "", onValueChange = {/*TODO*/ })
+        }
+        Row {
+            Text(text = "Port")
+            TextField(value = "", onValueChange = {/*TODO*/ })
+        }
+    }
+}
