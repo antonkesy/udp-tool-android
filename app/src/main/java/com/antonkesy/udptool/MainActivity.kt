996d327be6e77b867f0657ab57280be1d2fd50bb
Anton Kesy <anton@kesy.de>
2021-09-01 22:04:05 +0200

    Add floating action button for toggle listening

--- app/src/main/java/com/antonkesy/udptool/MainActivity.kt
+++ app/src/main/java/com/antonkesy/udptool/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.antonkesy.udptool.ui.CardList
 import com.antonkesy.udptool.ui.theme.UDPToolTheme
@@ -32,8 +35,18 @@ fun DefaultPreview() {
 @Composable
 fun MainView() {
     UDPToolTheme {
-        Scaffold { innerPadding ->
+        Scaffold(floatingActionButton = { StartStopFloatingActionButton() }) { innerPadding ->
             CardList(paddingValues = innerPadding)
         }
+    }
+}
+
+@Composable
+fun StartStopFloatingActionButton() {
+    FloatingActionButton(onClick = { /*TODO*/ }) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_baseline_play_arrow_24),
+            contentDescription = "start listening"
+        )
     }
 }
