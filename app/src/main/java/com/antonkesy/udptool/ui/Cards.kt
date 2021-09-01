633cde4d3f6febca57c34318c3440d1123a790f3
Anton Kesy <anton@kesy.de>
2021-09-01 21:51:08 +0200

    Revert "Remove unnecessary column"
    This reverts commit ec441941d49504539716d4f3a641c669209d0ddc.

--- app/src/main/java/com/antonkesy/udptool/ui/Cards.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/Cards.kt
@@ -24,7 +24,7 @@ fun CardList(paddingValues: PaddingValues) {
             .padding(paddingValues = paddingValues),
         verticalArrangement = Arrangement.spacedBy(3.dp)
     ) {
-        Column(Modifier.fillMaxHeight()) {
+        Column(Modifier.wrapContentHeight()) {
             val modifierCardPadding = Modifier.padding(15.dp)
             CardListCard(
                 label = "Device",
@@ -35,6 +35,12 @@ fun CardList(paddingValues: PaddingValues) {
             CardListCard(
                 label = "Messages",
                 content = { MessagesCardContent(label = "Messages", modifierCardPadding) })
+        }
+        Column(
+            Modifier
+                .padding(15.dp)
+                .fillMaxHeight()
+        ) {
             MessagesLogList()
         }
     }
