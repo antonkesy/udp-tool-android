2fcf63a741d9c6ff3c2a3667a3657d873ba7d66f
Anton Kesy <anton@kesy.de>
2021-09-01 21:30:58 +0200

    Update MessageLogView.kt

--- app/src/main/java/com/antonkesy/udptool/ui/MessageLogView.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/MessageLogView.kt
@@ -1,20 +1,26 @@
 package com.antonkesy.udptool.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.antonkesy.udptool.data.DeviceLogMessage
 import com.antonkesy.udptool.data.ILogMessage
+import java.text.SimpleDateFormat
 
 
 @Composable
 fun MessagesLogList() {
-    val messages = mutableListOf<ILogMessage>(
-        DeviceLogMessage("message 1"),
-        DeviceLogMessage("message 2"),
-        DeviceLogMessage("message 3")
-    )
-    LazyColumn() {
+    val messages = mutableListOf<ILogMessage>()
+    for (i in 1..100) {
+        messages.add(DeviceLogMessage("message $i"))
+    }
+
+    LazyColumn(reverseLayout = true) {
         items(items = messages) {
             MessageLogItem(it)
         }
@@ -24,6 +30,12 @@ fun MessagesLogList() {
 
 @Composable
 fun MessageLogItem(message: ILogMessage) {
-
+    Row(
+        Modifier.clickable { /*TODO(open detail view)*/ },
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        val formatter = SimpleDateFormat("HH:mm:ss.SSS")
+        Text(text = formatter.format(message.time) + ":")
+        Text(text = message.message)
+    }
 }
-
