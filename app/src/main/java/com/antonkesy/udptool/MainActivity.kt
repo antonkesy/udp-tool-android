ce497f1490745e4bac8bd53b0c9667928c9cc2b0
Anton Kesy <anton@kesy.de>
2021-09-01 20:24:26 +0200

    Extend card button

--- app/src/main/java/com/antonkesy/udptool/MainActivity.kt
+++ app/src/main/java/com/antonkesy/udptool/MainActivity.kt
@@ -3,10 +3,14 @@ package com.antonkesy.udptool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,9 +50,15 @@ fun CardList(paddingValues: PaddingValues) {
     ) {
         Column(Modifier.weight(1.0f, false)) {
             val modifierCardPadding = Modifier.padding(15.dp)
-            CardListCard(content = { IPConfigCardContent(modifierCardPadding) })
-            CardListCard(content = { RemoteContent(modifierCardPadding) })
-            CardListCard(content = { MessagesCardContent(modifierCardPadding) })
+            CardListCard(
+                label = "Device",
+                content = { IPConfigCardContent(label = "Device", modifierCardPadding) })
+            CardListCard(
+                label = "Remote",
+                content = { RemoteContent(label = "Device", modifierCardPadding) })
+            CardListCard(
+                label = "Messages",
+                content = { MessagesCardContent(label = "Device", modifierCardPadding) })
         }
         Column() {
             MessagesList()
@@ -67,9 +77,9 @@ fun MessagesList() {
 }
 
 @Composable
-fun IPConfigCardContent(modifier: Modifier) {
+fun IPConfigCardContent(label: String, modifier: Modifier) {
     Column(modifier) {
-        Text("Your device")
+        Text(label)
         Text("IP: 0.0.0.0")
         Text("Gateway: 0.0.0.0")
         Text("Network type: LAN")
@@ -82,9 +92,9 @@ fun IPConfigCardContent(modifier: Modifier) {
 
 
 @Composable
-fun RemoteContent(modifier: Modifier) {
+fun RemoteContent(label: String, modifier: Modifier) {
     Column(modifier = modifier) {
-        Text(text = "Remote")
+        Text(label)
         Row() {
             Text(text = "IP")
             TextField(value = "", onValueChange = {/*TODO*/ })
@@ -97,9 +107,9 @@ fun RemoteContent(modifier: Modifier) {
 }
 
 @Composable
-fun MessagesCardContent(modifier: Modifier) {
+fun MessagesCardContent(label: String, modifier: Modifier) {
     Column(modifier = modifier) {
-        Text(text = "Messages")
+        Text(label)
         Row {
             TextField(value = "", onValueChange = {/*TODO*/ })
             Button(onClick = { /*TODO*/ }) {
@@ -110,19 +120,42 @@ fun MessagesCardContent(modifier: Modifier) {
 }
 
 @Composable
-fun CardListCard(content: @Composable () -> Unit) {
+fun CardListCard(label: String, content: @Composable () -> Unit) {
     var isExtended by remember { mutableStateOf(true) }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(5.dp),
+            .padding(5.dp)
+            .clickable {
+                isExtended = !isExtended
+            },
         elevation = 10.dp
     ) {
         Column {
             if (isExtended) {
                 content()
             }
-            Button(onClick = { isExtended = !isExtended }) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Row(horizontalArrangement = Arrangement.Start) {
+                    if (!isExtended) {
+                        Text(label)
+                    }
+                }
+                IconButton(
+                    onClick = { isExtended = !isExtended },
+                ) {
+                    Icon(
+                        if (isExtended)
+                            Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                        contentDescription = null,
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                }
             }
         }
     }
