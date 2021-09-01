1d4748a16e60cda6ed8aed6ad4faa0d0b7c3722e
Anton Kesy <anton@kesy.de>
2021-09-01 22:00:56 +0200

    Split time and message

--- app/src/main/java/com/antonkesy/udptool/ui/MessageLogView.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/MessageLogView.kt
@@ -3,11 +3,15 @@ package com.antonkesy.udptool.ui
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.antonkesy.udptool.data.DeviceLogMessage
 import com.antonkesy.udptool.data.ILogMessage
 import java.text.SimpleDateFormat
@@ -31,11 +35,20 @@ fun MessagesLogList() {
 @Composable
 fun MessageLogItem(message: ILogMessage) {
     Row(
-        Modifier.clickable { /*TODO(open detail view)*/ },
+        Modifier
+            .clickable { /*TODO(open detail view)*/ }
+            .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         val formatter = SimpleDateFormat("HH:mm:ss.SSS")
-        Text(text = formatter.format(message.time) + ":")
-        Text(text = message.message)
+        Text(text = formatter.format(message.time) + ":", textAlign = TextAlign.Start, maxLines = 1)
+        Text(
+            text = message.message,
+            textAlign = TextAlign.Start,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp)
+        )
     }
 }
