c7fe77f760c829d403ecc2339aee3e411f219121
Anton Kesy <anton@kesy.de>
2021-09-02 11:18:14 +0200

    Create HelpDialog.kt

---
+++ app/src/main/java/com/antonkesy/udptool/ui/HelpDialog.kt
@@ -0,0 +1,60 @@
+package com.antonkesy.udptool.ui
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Help
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HelpDialogBoxButton(
+    dialogTitle: String,
+    dialogText: String
+) {
+    var isDialogShown by remember { mutableStateOf(false) }
+    IconButton(
+        onClick = { isDialogShown = true },
+    ) {
+        Icon(
+            Icons.Filled.Help,
+            contentDescription = null,
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+    }
+    if (isDialogShown) {
+        HelpDialogBox(
+            dialogTitle = dialogTitle,
+            dialogText = dialogText,
+            onConfirm = { isDialogShown = false })
+    }
+}
+
+@Composable
+fun HelpDialogBox(
+    dialogTitle: String,
+    dialogText: String,
+    onConfirm: () -> Unit
+) {
+
+    AlertDialog(
+        onDismissRequest = {
+        },
+        title = {
+            Text(dialogTitle)
+        },
+        confirmButton = {
+            Button(
+                onClick = {
+                    onConfirm()
+                },
+            ) {
+                Text("Ok :)")
+            }
+        },
+        text = {
+            Text(dialogText)
+        },
+    )
+}
+
