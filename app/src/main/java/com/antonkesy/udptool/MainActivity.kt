14bde793ed93fa2a865cb78c3e087a3088b260d6
Anton Kesy <anton@kesy.de>
2021-09-02 19:30:41 +0200

    generate test messages only once

--- app/src/main/java/com/antonkesy/udptool/MainActivity.kt
+++ app/src/main/java/com/antonkesy/udptool/MainActivity.kt
@@ -25,6 +25,8 @@ class MainActivity : ComponentActivity() {
 
         val messageViewModel: MessageLogViewModel by viewModels()
 
+        messageViewModel.generateTestLogMessages()
+
         setContent {
             MainView(messageViewModel)
         }
