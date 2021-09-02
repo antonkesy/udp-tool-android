74d07176effe4bdd577d5f80eeb7960f05409333
Anton Kesy <anton@kesy.de>
2021-09-02 10:47:09 +0200

    Update DeviceCard.kt

--- app/src/main/java/com/antonkesy/udptool/ui/cards/DeviceCard.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/cards/DeviceCard.kt
@@ -1,11 +1,23 @@
 package com.antonkesy.udptool.ui.cards
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.antonkesy.udptool.ui.NumberOutlinedTextField
+
+@ExperimentalAnimationApi
+@Composable
+fun DeviceCard() {
+    CardListCard(
+        label = "Device",
+        content = {
+            IPConfigCardContent {/*TODO*/ true }
+        }, dialogText = "text"
+    )
+}
 
 @Composable
 fun IPConfigCardContent(
