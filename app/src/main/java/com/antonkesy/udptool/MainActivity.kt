e6c97554ce30d7b71937e75e839732abb361c086
Anton Kesy <anton@kesy.de>
2021-09-02 18:49:37 +0200

    remove unused floating action button

--- app/src/main/java/com/antonkesy/udptool/MainActivity.kt
+++ app/src/main/java/com/antonkesy/udptool/MainActivity.kt
@@ -3,12 +3,8 @@ package com.antonkesy.udptool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.antonkesy.udptool.ui.BottomNavigationWithOnlySelectedLabels
@@ -49,18 +45,8 @@ fun MainView() {
                 items = bottomNavigationItems,
                 navController = navController
             )
-        }, floatingActionButton = { StartStopFloatingActionButton() }) { innerPadding ->
+        }) { innerPadding ->
             Navigation(navController = navController, innerPadding = innerPadding)
         }
-    }
-}
-
-@Composable
-fun StartStopFloatingActionButton() {
-    FloatingActionButton(onClick = { /*TODO*/ }) {
-        Icon(
-            painter = painterResource(id = R.drawable.ic_baseline_play_arrow_24),
-            contentDescription = "start listening"
-        )
     }
 }
