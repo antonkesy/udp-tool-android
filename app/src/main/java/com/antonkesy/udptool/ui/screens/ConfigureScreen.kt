55799c5f13bbe28805d14ee3d53734a1b6d80121
Anton Kesy <anton@kesy.de>
2021-09-02 19:36:57 +0200

    Convert to lazy column

--- app/src/main/java/com/antonkesy/udptool/ui/screens/ConfigureScreen.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/screens/ConfigureScreen.kt
@@ -1,11 +1,9 @@
 package com.antonkesy.udptool.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -16,15 +14,16 @@ import com.antonkesy.udptool.ui.cards.ToggleLogCard
 
 @Composable
 fun ConfigureScreen(paddingValues: PaddingValues) {
-    Column(
+    LazyColumn(
         Modifier
-            .padding(paddingValues = paddingValues)
-            .verticalScroll(rememberScrollState()),
+            .padding(paddingValues = paddingValues),
         verticalArrangement = Arrangement.spacedBy(3.dp)
     ) {
-        DeviceCard()
-        RemoteCard()
-        MessageCard()
-        ToggleLogCard()
+        item {
+            DeviceCard()
+            RemoteCard()
+            MessageCard()
+            ToggleLogCard()
+        }
     }
 }
