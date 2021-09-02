7bb7a94f46c85be4bff0e7a5b2fcd0d55636a35c
Anton Kesy <anton@kesy.de>
2021-09-02 18:37:26 +0200

    Create LogScreen.kt

---
+++ app/src/main/java/com/antonkesy/udptool/ui/screens/LogScreen.kt
@@ -0,0 +1,20 @@
+package com.antonkesy.udptool.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.antonkesy.udptool.ui.cards.MessagesLogList
+
+@Composable
+fun LogScreen(paddingValues: PaddingValues) {
+    Column(
+        Modifier
+            .padding(paddingValues)
+            .fillMaxSize()
+    ) {
+        MessagesLogList()
+    }
+}
