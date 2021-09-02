b9d5e6e9a52a4519cadd564400bfa03fece39325
Anton Kesy <anton@kesy.de>
2021-09-02 19:26:43 +0200

    Update SplashScreen.kt

--- app/src/main/java/com/antonkesy/udptool/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/screens/SplashScreen.kt
@@ -2,9 +2,8 @@ package com.antonkesy.udptool.ui.screens
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 
 @Composable
-fun SplashScreen(navController: NavHostController, nextStartRoute: String) {
+fun SplashScreen() {
     Text("UDP Tool")
 }
