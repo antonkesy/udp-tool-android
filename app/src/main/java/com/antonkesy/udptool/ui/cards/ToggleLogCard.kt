cf8152d1f9011fb2bc8c51704ca5346b1c22efdf
Anton Kesy <anton@kesy.de>
2021-09-04 15:59:21 +0200

    Update ToggleLogCard.kt

--- app/src/main/java/com/antonkesy/udptool/ui/cards/ToggleLogCard.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/cards/ToggleLogCard.kt
@@ -2,14 +2,17 @@ package com.antonkesy.udptool.ui.cards
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.antonkesy.udptool.ui.NumberOutlinedTextField
+import com.antonkesy.udptool.ui.log.MessageLogViewModel
 
 @Composable
-fun ToggleLogCard() {
+fun ToggleLogCard(logViewModel: MessageLogViewModel) {
     val label = "Logging"
     CardListCard(
         label = label,
@@ -18,7 +21,8 @@ fun ToggleLogCard() {
             var isLogging by remember { mutableStateOf(false) }
             ToggleLogCardContent(
                 isLogging = isLogging,
-                onLoggingToggleClick = { isLogging = !isLogging }
+                onLoggingToggleClick = { isLogging = !isLogging },
+                viewModel = logViewModel
             )
         }
     )
@@ -27,20 +31,38 @@ fun ToggleLogCard() {
 @Composable
 fun ToggleLogCardContent(
     isLogging: Boolean,
-    onLoggingToggleClick: () -> Unit
+    onLoggingToggleClick: () -> Unit, viewModel: MessageLogViewModel
 ) {
     Column(Modifier.fillMaxWidth()) {
-        Button(onClick = { onLoggingToggleClick() }) {
-            if (isLogging) {
-                Text(text = "stop logging")
-            } else {
-                Text(text = "start logging")
-            }
-        }
-        NumberOutlinedTextField(
-            label = "buffer size (byte)",
-            isErrorOnOutlineTextFieldValueChange = { !isStringLegalBufferSize(it) })
+        SetBufferSize(viewModel)
+        ToggleLoggingButton(isLogging = isLogging, onLoggingToggleClick = onLoggingToggleClick)
     }
+}
+
+@Composable
+fun ToggleLoggingButton(isLogging: Boolean, onLoggingToggleClick: () -> Unit) {
+    Button(onClick = { onLoggingToggleClick() }, Modifier.padding(top = 16.dp)) {
+        if (isLogging) {
+            Text(text = "stop logging")
+        } else {
+            Text(text = "start logging")
+        }
+    }
+}
+
+@Composable
+fun SetBufferSize(viewModel: MessageLogViewModel) {
+    NumberOutlinedTextField(
+        label = "buffer size (byte)",
+        value = viewModel.bufferSize.value.toString(),
+        isErrorOnOutlineTextFieldValueChange = {
+            if (isStringLegalBufferSize(it)) {
+                viewModel.setBufferSize(Integer.parseInt(it))
+                return@NumberOutlinedTextField false
+            } else {
+                return@NumberOutlinedTextField true
+            }
+        })
 }
 
 fun isStringLegalBufferSize(input: String): Boolean {
