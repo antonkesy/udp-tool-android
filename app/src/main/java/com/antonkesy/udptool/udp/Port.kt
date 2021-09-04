c027bb058911864068f71b8a1a1979fb8486e3db
Anton Kesy <anton@kesy.de>
2021-09-04 16:36:45 +0200

    Create Port.kt

---
+++ app/src/main/java/com/antonkesy/udptool/udp/Port.kt
@@ -0,0 +1,33 @@
+package com.antonkesy.udptool.udp
+
+import com.antonkesy.udptool.ui.log.MessageLogViewModel
+
+fun isLegalPort(value: String): Boolean {
+    try {
+        if (Integer.parseInt(value) in 0..65_535) {
+            return true
+        }
+    } catch (e: NumberFormatException) {
+    }
+    return false
+}
+
+fun getPort(value: String): Int {
+    return Integer.parseInt(value)
+}
+
+fun setNewLocalPort(value: String, logViewModel: MessageLogViewModel): Boolean {
+    if (isLegalPort(value = value)) {
+        logViewModel.setLocalPort(getPort(value = value))
+        return false
+    }
+    return true
+}
+
+fun setNewRemotePort(value: String, logViewModel: MessageLogViewModel): Boolean {
+    if (isLegalPort(value = value)) {
+        logViewModel.setRemotePort(getPort(value = value))
+        return false
+    }
+    return true
+}
