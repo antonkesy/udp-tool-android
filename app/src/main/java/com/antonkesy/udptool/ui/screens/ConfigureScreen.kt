1ffba999e6ba7dc49c47ab6a4503feae7d40a6cf
Anton Kesy <anton@kesy.de>
2021-09-04 11:25:20 +0200

    Update ConfigureScreen.kt

--- app/src/main/java/com/antonkesy/udptool/ui/screens/ConfigureScreen.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/screens/ConfigureScreen.kt
@@ -7,21 +7,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.antonkesy.udptool.ui.cards.DeviceCard
-import com.antonkesy.udptool.ui.cards.MessageCard
-import com.antonkesy.udptool.ui.cards.RemoteCard
-import com.antonkesy.udptool.ui.cards.ToggleLogCard
+import com.antonkesy.udptool.ui.cards.*
 import com.antonkesy.udptool.ui.log.MessageLogViewModel
 
 @Composable
-fun ConfigureScreen(paddingValues: PaddingValues,logViewModel: MessageLogViewModel) {
+fun ConfigureScreen(paddingValues: PaddingValues, logViewModel: MessageLogViewModel) {
     LazyColumn(
         Modifier
             .padding(paddingValues = paddingValues),
         verticalArrangement = Arrangement.spacedBy(3.dp)
     ) {
         item {
-            DeviceCard()
+            DeviceCard(info = DeviceInfo("0.0.0.0", "0.0.0.0", "LAN"), logViewModel = logViewModel)
             RemoteCard()
             MessageCard(logViewModel)
             ToggleLogCard()
