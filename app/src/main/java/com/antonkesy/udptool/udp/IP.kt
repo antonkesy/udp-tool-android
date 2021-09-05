79be86fb07f4689e071593829c318b605c26d882
Anton Kesy <anton@kesy.de>
2021-09-05 10:56:19 +0200

    Update IP.kt

--- app/src/main/java/com/antonkesy/udptool/udp/IP.kt
+++ app/src/main/java/com/antonkesy/udptool/udp/IP.kt
@@ -1,27 +1,19 @@
 package com.antonkesy.udptool.udp
 
 import com.antonkesy.udptool.ui.log.MessageLogViewModel
+import java.net.Inet4Address
 
 fun isLegalIP(value: String): Boolean {
-    //TODO
     try {
-        if (Integer.parseInt(value) in 0..65_535) {
-            return true
-        }
-    } catch (e: NumberFormatException) {
+        return Inet4Address.getByName(value).hostAddress.equals(value)
+    } catch (e: Exception) {
     }
     return false
 }
 
-
-fun getIP(value: String): String {
-    //TODO
-    return value
-}
-
 fun setNewRemoteIP(value: String, logViewModel: MessageLogViewModel): Boolean {
-    if (isLegalPort(value = value)) {
-        logViewModel.setRemoteIP(getIP(value = value))
+    if (isLegalIP(value = value)) {
+        logViewModel.setRemoteIP(value)
         return false
     }
     return true
