9122fa9dd5068b3a77d1eeaa6cd82e7307a254e4
Anton Kesy <anton@kesy.de>
2021-09-05 10:41:36 +0200

    Save and load user data

--- app/src/main/java/com/antonkesy/udptool/data/store/UserSaveData.kt
+++ app/src/main/java/com/antonkesy/udptool/data/store/UserSaveData.kt
@@ -3,6 +3,10 @@ package com.antonkesy.udptool.data.store
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.*
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import com.antonkesy.udptool.ui.log.ASCII
+import com.antonkesy.udptool.ui.log.HEX
+import com.antonkesy.udptool.ui.log.LogMessageCoding
 import com.antonkesy.udptool.ui.log.MessageLogViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
@@ -20,7 +24,16 @@ private val MESSAGE_CODING = intPreferencesKey("message_coding")
 
 internal fun loadSavedData(viewModel: MessageLogViewModel, dataStore: DataStore<Preferences>) {
     runBlocking {
-        viewModel.setLocalPort(getLocalPort(dataStore))
+        with(viewModel) {
+            setLocalPort(getLocalPort(dataStore))
+            setRemotePort(getRemotePort(dataStore))
+            setRemoteIP(getRemoteIP(dataStore))
+            setTimeOutTime(getTimeOutTime(dataStore))
+            setIsTimeOutTime(getIsTimeOut(dataStore))
+            setIsMessage(getIsMessage(dataStore))
+            setBufferSize(getBufferSize(dataStore))
+            setMessageCoding(getMessageCoding(dataStore))
+        }
     }
 }
 
@@ -29,20 +42,104 @@ internal fun observeToSaveData(
     viewModel: MessageLogViewModel,
     dataStore: DataStore<Preferences>
 ) {
-    viewModel.localPort.observe(lifecycleOwner, {
-        runBlocking {
-            setLocalPort(it, dataStore)
+    observeData(
+        lifecycleOwner = lifecycleOwner,
+        liveData = viewModel.localPort,
+        {
+            runBlocking {
+                setLocalPort(it, dataStore)
+            }
         }
+    )
+
+    observeData(
+        lifecycleOwner = lifecycleOwner,
+        liveData = viewModel.remotePort,
+        {
+            runBlocking {
+                setRemotePort(it, dataStore)
+            }
+        }
+    )
+
+    observeData(
+        lifecycleOwner = lifecycleOwner,
+        liveData = viewModel.remoteIP,
+        {
+            runBlocking {
+                setRemoteIP(it, dataStore)
+            }
+        }
+    )
+
+    observeData(
+        lifecycleOwner = lifecycleOwner,
+        liveData = viewModel.timeOutTime,
+        {
+            runBlocking {
+                setTimeOutTime(it, dataStore)
+            }
+        }
+    )
+
+    observeData(
+        lifecycleOwner = lifecycleOwner,
+        liveData = viewModel.isTimeOutTime,
+        {
+            runBlocking {
+                setIsTimeOut(it, dataStore)
+            }
+        }
+    )
+
+    observeData(
+        lifecycleOwner = lifecycleOwner,
+        liveData = viewModel.isMessage,
+        {
+            runBlocking {
+                setIsMessage(it, dataStore)
+            }
+        }
+    )
+
+    observeData(
+        lifecycleOwner = lifecycleOwner,
+        liveData = viewModel.bufferSize,
+        {
+            runBlocking {
+                setBufferSize(it, dataStore)
+            }
+        }
+    )
+
+    observeData(
+        lifecycleOwner = lifecycleOwner,
+        liveData = viewModel.messageCoding,
+        {
+            runBlocking {
+                setMessageCoding(it, dataStore)
+            }
+        }
+    )
+}
+
+private fun <T> observeData(
+    lifecycleOwner: LifecycleOwner,
+    liveData: LiveData<T>,
+    setFunction: (T) -> Unit,
+) {
+    liveData.observe(lifecycleOwner, {
+        setFunction(it)
     })
 }
 
-private suspend fun getInt(
-    key: Preferences.Key<Int>,
-    dataStore: DataStore<Preferences>, default: Int = 0
-): Int {
+private suspend fun <T> getData(
+    key: Preferences.Key<T>,
+    dataStore: DataStore<Preferences>, default: T
+): T {
     var value = default
 
-    val exampleCounterFlow: Flow<Int> = dataStore.data
+    val exampleCounterFlow: Flow<T> = dataStore.data
         .map { preferences ->
             preferences[key] ?: default
         }
@@ -54,9 +151,9 @@ private suspend fun getInt(
     return value
 }
 
-private suspend fun setInt(
-    newValue: Int,
-    key: Preferences.Key<Int>,
+private suspend fun <T> setData(
+    newValue: T,
+    key: Preferences.Key<T>,
     dataStore: DataStore<Preferences>
 ) {
     dataStore.edit { settings ->
@@ -65,9 +162,77 @@ private suspend fun setInt(
 }
 
 private suspend fun getLocalPort(dataStore: DataStore<Preferences>): Int {
-    return getInt(LOCAL_PORT, dataStore)
+    return getData(LOCAL_PORT, dataStore, 0)
 }
 
 private suspend fun setLocalPort(newValue: Int, dataStore: DataStore<Preferences>) {
-    setInt(newValue, LOCAL_PORT, dataStore)
+    setData(newValue, LOCAL_PORT, dataStore)
+}
+
+private suspend fun getRemotePort(dataStore: DataStore<Preferences>): Int {
+    return getData(REMOTE_PORT, dataStore, 0)
+}
+
+private suspend fun setRemotePort(newValue: Int, dataStore: DataStore<Preferences>) {
+    setData(newValue, REMOTE_PORT, dataStore)
+}
+
+private suspend fun getTimeOutTime(dataStore: DataStore<Preferences>): Int {
+    return getData(TIMEOUT_TIME, dataStore, 0)
+}
+
+private suspend fun setTimeOutTime(newValue: Int, dataStore: DataStore<Preferences>) {
+    setData(newValue, TIMEOUT_TIME, dataStore)
+}
+
+private suspend fun getBufferSize(dataStore: DataStore<Preferences>): Int {
+    return getData(BUFFER_SIZE, dataStore, 0)
+}
+
+private suspend fun setBufferSize(newValue: Int, dataStore: DataStore<Preferences>) {
+    setData(newValue, BUFFER_SIZE, dataStore)
+}
+
+private suspend fun getMessageCoding(dataStore: DataStore<Preferences>): LogMessageCoding {
+    return when (getData(MESSAGE_CODING, dataStore, 0)) {
+        1 -> ASCII
+        2 -> HEX
+        else -> ASCII
+    }
+}
+
+private suspend fun setMessageCoding(
+    newValue: LogMessageCoding,
+    dataStore: DataStore<Preferences>
+) {
+    setData(
+        when (newValue) {
+            ASCII -> 1
+            HEX -> 2
+        }, MESSAGE_CODING, dataStore
+    )
+}
+
+private suspend fun getRemoteIP(dataStore: DataStore<Preferences>): String {
+    return getData(REMOTE_IP, dataStore, "0.0.0.0")
+}
+
+private suspend fun setRemoteIP(newValue: String, dataStore: DataStore<Preferences>) {
+    setData(newValue, REMOTE_IP, dataStore)
+}
+
+private suspend fun getIsTimeOut(dataStore: DataStore<Preferences>): Boolean {
+    return getData(IS_TIMEOUT, dataStore, false)
+}
+
+private suspend fun setIsTimeOut(newValue: Boolean, dataStore: DataStore<Preferences>) {
+    setData(newValue, IS_TIMEOUT, dataStore)
+}
+
+private suspend fun getIsMessage(dataStore: DataStore<Preferences>): Boolean {
+    return getData(IS_MESSAGE, dataStore, false)
+}
+
+private suspend fun setIsMessage(newValue: Boolean, dataStore: DataStore<Preferences>) {
+    setData(newValue, IS_MESSAGE, dataStore)
 }
