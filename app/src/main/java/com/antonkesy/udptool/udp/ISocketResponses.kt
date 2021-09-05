8ac1edbc1d7331cc363763f07e9d0b46454233f7
Anton Kesy <anton@kesy.de>
2021-09-05 19:43:37 +0200

    Update ISocketResponses.kt

--- app/src/main/java/com/antonkesy/udptool/udp/ISocketResponses.kt
+++ app/src/main/java/com/antonkesy/udptool/udp/ISocketResponses.kt
@@ -5,4 +5,7 @@ interface ISocketResponses {
     fun ioException()
     fun socketException()
     fun dataReceived(data: ByteArray)
+    fun socketStart()
+    fun socketClosed()
+    fun sendPacket(data: ByteArray)
 }
