e5d0392c5d6c8fba7edbd1b3c7c9347e5ccc5809
Anton Kesy <anton@kesy.de>
2021-09-08 12:56:09 +0200

    switch between hex and ascii coding

--- app/src/main/java/com/antonkesy/udptool/ui/dialogs/DetailDialog.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/dialogs/DetailDialog.kt
@@ -14,9 +14,18 @@ import com.antonkesy.udptool.messages.DeviceLogMessage
 import com.antonkesy.udptool.messages.ILogMessage
 import com.antonkesy.udptool.messages.MessageLog
 import com.antonkesy.udptool.messages.SocketLogMessage
+import com.antonkesy.udptool.ui.log.ASCII
+import com.antonkesy.udptool.ui.log.HEX
+import com.antonkesy.udptool.ui.log.MessageLogViewModel
+import com.antonkesy.udptool.util.getDataAsASCIIString
+import com.antonkesy.udptool.util.getDataAsHexString
 
 @Composable
-fun DetailDialog(logMessage: ILogMessage, showDetailDialog: MutableState<Boolean>) {
+fun DetailDialog(
+    logMessage: ILogMessage,
+    showDetailDialog: MutableState<Boolean>,
+    viewModel: MessageLogViewModel
+) {
     if (showDetailDialog.value) {
         AlertDialog(
             onDismissRequest = {
@@ -35,48 +44,60 @@ fun DetailDialog(logMessage: ILogMessage, showDetailDialog: MutableState<Boolean
                 }
             },
             text = {
-                DetailDialogContent(logMessage)
+                DetailDialogContent(logMessage, viewModel = viewModel)
             }
         )
     }
 }
 
 @Composable
-fun DetailDialogContent(logMessage: ILogMessage) {
+fun DetailDialogContent(logMessage: ILogMessage, viewModel: MessageLogViewModel) {
     Column(Modifier.fillMaxWidth()) {
         Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
             Text(text = "Time: ")
             Text(text = logMessage.time)
         }
-        DetailContent(logMessage = logMessage)
+        DetailContent(logMessage = logMessage, viewModel = viewModel)
     }
 }
 
 @Composable
-fun DetailContent(logMessage: ILogMessage) {
+fun DetailContent(logMessage: ILogMessage, viewModel: MessageLogViewModel) {
     when (logMessage) {
         is DeviceLogMessage -> DetailContentDeviceLog(logMessage = logMessage)
         is SocketLogMessage -> DetailContentSocketLog(logMessage = logMessage)
-        is MessageLog -> DetailContentMessageLog(logMessage = logMessage)
+        is MessageLog -> DetailContentMessageLog(logMessage = logMessage, viewModel = viewModel)
     }
 }
 
 @Composable
-fun DetailContentDeviceLog(logMessage: ILogMessage) {
+fun DetailContentDeviceLog(logMessage: DeviceLogMessage) {
     Column(Modifier.fillMaxWidth()) {
         Text(logMessage.info)
     }
 }
 
 @Composable
-fun DetailContentMessageLog(logMessage: ILogMessage) {
+fun DetailContentMessageLog(logMessage: MessageLog, viewModel: MessageLogViewModel) {
     Column(Modifier.fillMaxWidth()) {
-        Text((logMessage as MessageLog).data.toString())
+        Text(text = "Message:")
+        val data = logMessage.data
+        Text(
+            when (viewModel.messageCoding.value) {
+                ASCII -> {
+                    getDataAsASCIIString(data)
+                }
+                HEX -> {
+                    getDataAsHexString(data)
+                }
+                else -> ""
+            }
+        )
     }
 }
 
 @Composable
-fun DetailContentSocketLog(logMessage: ILogMessage) {
+fun DetailContentSocketLog(logMessage: SocketLogMessage) {
     Column(Modifier.fillMaxWidth()) {
         Text(logMessage.info)
     }
