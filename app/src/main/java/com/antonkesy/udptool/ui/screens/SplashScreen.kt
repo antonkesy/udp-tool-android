54c69ddd6036f3662faf7a35cbd8fcab2b5c853c
Anton Kesy <anton@kesy.de>
2021-09-08 18:15:55 +0200

    Update SplashScreen.kt

--- app/src/main/java/com/antonkesy/udptool/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/antonkesy/udptool/ui/screens/SplashScreen.kt
@@ -1,9 +1,28 @@
 package com.antonkesy.udptool.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import com.antonkesy.udptool.R
 
 @Composable
 fun SplashScreen() {
-    Text("UDP Tool")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("UDP Tool", fontWeight = FontWeight.ExtraBold)
+        Icon(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "logo"
+        )
+    }
 }
