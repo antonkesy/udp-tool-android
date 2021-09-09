47908b801e658cbf4394b537fec47247761cd9e8
Anton Kesy <anton@kesy.de>
2021-09-09 09:50:44 +0200

    Update SocketLogMessage.kt

--- app/src/main/java/com/antonkesy/udptool/messages/SocketLogMessage.kt
+++ app/src/main/java/com/antonkesy/udptool/messages/SocketLogMessage.kt
@@ -2,23 +2,25 @@ package com.antonkesy.udptool.messages
 
 import com.antonkesy.udptool.util.getCurrentTimeAsStringDate
 
-sealed class SocketLogMessage(
+enum class SocketLogMessageType {
+    CLOSED, START, EXCEPTION, TIMEOUT, IOEXCEPTION
+}
+
+data class SocketLogMessage(
     override val title: String,
     override val info: String,
     override val time: String
 ) :
     ILogMessage {
-    constructor(title: String, message: String) : this(
-        title = title,
+    constructor(type: SocketLogMessageType, message: String) : this(
+        title = when (type) {
+            SocketLogMessageType.CLOSED -> "socket closed"
+            SocketLogMessageType.START -> "socket start"
+            SocketLogMessageType.EXCEPTION -> "socket exception"
+            SocketLogMessageType.TIMEOUT -> "socket timeout"
+            SocketLogMessageType.IOEXCEPTION -> "socket ioexception"
+        },
         info = message,
         time = getCurrentTimeAsStringDate()
     )
-
-    object CLOSED : SocketLogMessage("socket closed", "")
-    object START : SocketLogMessage("socket start", "")
-    object SEND : SocketLogMessage("socket send", "")
-    object RECEIVED : SocketLogMessage("socket received", "")
-    object EXCEPTION : SocketLogMessage("socket exception", "")
-    object TIMEOUT : SocketLogMessage("socket timeout", "")
-    object IOEXCEPTION : SocketLogMessage("socket ioexception", "")
 }
